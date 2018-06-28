3a43eb74e3d79d0678c9ea35c504741fd0f10f9a
John Borland <john.borland@gmail.com>
2018-06-28 22:25:40 +0200

    scheduler

---
+++ app/scheduler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+from datetime import datetime
+import os
+from time import sleep
+from apscheduler.schedulers.blocking import BlockingScheduler
+import pollchat_twitterscrape_timed as pt
+
+sched = BlockingScheduler()
+
+
+
+
+
+def main():
+
+
+    job = sched.add_job(pt.run_twitterscrape, 'cron', hour=22, minute=30)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
