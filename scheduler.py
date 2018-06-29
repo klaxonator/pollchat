faebe3661f57737567ccb5f4b503422d592122d2
John Borland <john.borland@gmail.com>
2018-06-29 09:02:07 +0200

    sked

--- scheduler.py
+++ scheduler.py
@@ -16,7 +16,7 @@ import pollchat_twitterscrape_timed as pt
 def main():
     sched = BlockingScheduler()
 
-    job = sched.add_job(pt.run_twitterscrape, 'interval', hours=24)
+    job = sched.add_job(pt.run_twitterscrape, 'cron', hour=7, minute=5)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
