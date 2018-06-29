493d41c36c713fd0c437440a9e01cfffe3d0a98e
John Borland <john.borland@gmail.com>
2018-06-29 10:48:30 +0200

    sked

--- pollchat_twitterscrape_timed.py
+++ pollchat_twitterscrape_timed.py
@@ -291,7 +291,7 @@ def twitter_search(query):
 
 def run_twitterscrape():
     with open('logs/twitterscrape_log.txt', 'a') as fw:
-        fw.write('started twitterscrape')
+        fw.write('started twitterscrape\n')
 
     # #instantiate db engine
     # engine = create_engine(os.environ.get('DATABASE_URL'))   #FOR NOW:
