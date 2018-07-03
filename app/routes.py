836e6a47e7b116c09311a03bbc0a453b6854be87
John Borland <john.borland@gmail.com>
2018-07-03 21:44:47 +0200

    routes typo

--- app/routes.py
+++ app/routes.py
@@ -285,11 +285,11 @@ def overview(dynamic):
 
     return render_template('overview.html', t_form=ChangeTimeForm(), \
     all_form=AllCongSearchForm(), dynamic=dynamic, time_delta=time_delta, \
-    url=url, all_hashes=all_hashes, top_tweeters=top_tweeters, \
-    retweeted_users=retweeted_users, hashes_no_dists=hashes_no_dists, \
+    url=url, all_hashes=all_hashes,  hashes_no_dists=hashes_no_dists, \
     hashtable_all=hashtable_all, all_tweets=all_tweets\
     # most_retweeted_tweets=most_retweeted_tweets, get_tweet=get_tweet, \
     # most_retweeted_tweet_list=most_retweeted_tweet_list\
+    # top_tweeters=top_tweeters, retweeted_users=retweeted_users,
     # most_active=most_active\
     )
 
