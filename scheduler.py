6139f1f524ca883f875ecfcf92122f29798a1cd4
John Borland <john.borland@gmail.com>
2018-07-17 11:54:39 +0200

    sked

--- scheduler.py
+++ scheduler.py
@@ -16,7 +16,7 @@ import pollchat_twitterscrape_timed as pt
 def main():
     sched = BlockingScheduler()
 
-    job = sched.add_job(pt.run_twitterscrape, 'cron', hour=11, minute=10,\
+    job = sched.add_job(pt.run_twitterscrape, 'cron', hour=9, minute=57,\
     id='scrape_one', replace_existing=True)
     job_two = sched.add_job(pt.run_twitterscrape, 'cron', hour=21, minute=30, \
     id='scrape_two', replace_existing=True)
